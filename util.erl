06c6592acc2518245998c2bf7dafaed253e67723
Gabriel Brosula <82014893+gabrielbrosula@users.noreply.github.com>
2025-12-01 04:46:40 -0500

    Fix erlang modulo implementation

--- util.erl
+++ util.erl
@@ -7,7 +7,11 @@
 % rem in Erlang != modulo for negative numbers
 % -10 rem 7 gives -3, while mod(-10, 7) should give 4, since -10 = -2 * 7 + 4.
 mod(X, Y) when X > 0 -> X rem Y;
-mod(X, Y) when X < 0 -> Y + X rem Y;
+
+% The additional rem operation is needed when X rem Y is 0.
+% This is because Y + X rem Y when X rem Y = 0 lets Y be part of the domain, when 
+% answers for modulo should always be in the range [0, Y).
+mod(X, Y) when X < 0 -> (Y + X rem Y) rem Y;
 mod(0, _) -> 0.
 
 read_file(FileName) -> 
