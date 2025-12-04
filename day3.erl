a4a41f1f48c6a8d718ada6a3d41e58c4f7038d44
Gabriel Brosula <82014893+gabrielbrosula@users.noreply.github.com>
2025-12-03 20:07:03 -0500

    Solve day3 part 2

--- day3.erl
+++ day3.erl
@@ -1,5 +1,5 @@
 -module(day3).
--export([p1/0]).
+-export([p1/0, p2/0, voltage/2]).
 
 p1() ->
     Lines = util:read_file("input/3.txt"),
@@ -25,3 +25,35 @@ p1() ->
     %io:format("~p~n", [Lines]),
     %io:format("~w~n", [Voltages]),
     erlang:display(lists:sum(Voltages)).
+
+p2() ->
+    Lines = util:read_file("input/3.txt"),
+    Voltages = get_voltages(Lines),
+    erlang:display(lists:sum(Voltages)).
+
+get_voltages(Lines) -> 
+    lists:foldl(
+        fun(Line, Acc) ->
+            Acc ++ [list_to_integer(voltage(Line, 12))]
+        end,
+        [],
+        Lines
+    ).
+
+% Largest subsequence of length K
+voltage(String, K) -> max_subsequence(String, K, []).
+
+max_subsequence(String, K, Acc) when length(String) > K, K > 0 ->
+    SearchStr = string:slice(String, 0, length(String) - K + 1),
+    Max = lists:max(SearchStr),
+    MaxIdx = util:find_index(Max, String),
+    NewStr = string:slice(String, MaxIdx + 1),
+    NewAcc = Acc ++ [Max],
+    % io:format("String: ~p, K: ~p, Acc: ~p, Max: ~c, MaxIdx: ~p, NewStr: ~p, NewAcc: ~p~n", [String, K, Acc, Max, MaxIdx, NewStr, NewAcc]),
+    max_subsequence(NewStr, K - 1, NewAcc);
+max_subsequence(String, K, Acc) when length(String) == K, K > 0 ->
+    Acc ++ String;
+max_subsequence(_, 0, Acc) -> Acc;
+max_subsequence(_, K, _) when K < 0 -> error.
+
+
